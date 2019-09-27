89c4da142a02c33d511d13b6c5fb1ff1898af003
tgaychey <tgaychey@gmail.com>
2019-09-27 03:02:33 -0400

    Updated Stripe test public key to live.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import React from "react"
 import { StripeProvider } from "react-stripe-elements"
 
 export const wrapRootElement = ({ element }) => (
-  <StripeProvider apiKey="pk_test_VM3CFtcuFgaY3PlZj3bUn4Rf">
+  <StripeProvider apiKey="pk_live_407So8bPh2e8ZqEbKJyRO5oJ">
     {element}
   </StripeProvider>
 )
